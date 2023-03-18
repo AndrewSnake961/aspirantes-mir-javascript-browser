871b3bf0e59aa9141fe6ee6831f6fd33b45cdb99
Andres <felipe.hbuiles@gmail.com>
2023-03-17 19:02:10 -0500

    Create index.html and script.js - Finish exercise 1(Button event)

---
+++ js-brow-1/script_1.js
@@ -0,0 +1,24 @@
+const input = document.querySelector('#texto');
+const resultado = document.querySelector('#resultado');
+const button = document.querySelector('.btn');
+const resultadoMayusculas = document.querySelector('#resultadoMayusculas');
+
+
+
+input.addEventListener('input', function() {
+    const texto = input.value// Código para convertir el texto a mayúsculas y mostrarlo en el párrafo
+    resultado.textContent = texto// Código para mostrar el texto en el párrafo
+    
+});
+
+button.addEventListener('click', function() {
+    const texto = input.value.toUpperCase();
+    resultadoMayusculas.textContent = texto;
+});
+
+
+
+
+
+
+
